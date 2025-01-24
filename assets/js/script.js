87c2027d21edf0b735b95abe017f276d90f57206
MathChewDoor <167593779+MathChewDoor@users.noreply.github.com>
2025-01-24 21:26:09 +0800

    feature(html): added info

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,5 @@ document.getElementById("changeColorBtn").addEventListener("click", function() {
     const colors = ['#F0E68C', '#ADD8E6', '#FFE4B5', '#FFD700', '#E0FFFF'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     document.body.style.backgroundColor = randomColor;
-    document.body.style.color = '#000000'; // Keep font color black
+    document.body.style.color = '#000000';
 });
